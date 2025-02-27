ba439e7824ec3f2ba3b1ab4738cc4b4fcb94cca6
Артем Сиднев <artemsidnev@MacBook-Pro-Artem.local>
2025-02-27 20:13:50 +0300

    Initial commit / Created Spring Application

---
+++ src/main/java/com/example/anti_spam_telegram_bot/AntiSpamTelegramBot.java
@@ -0,0 +1,44 @@
+package com.example.anti_spam_telegram_bot;
+
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+public class AntiSpamTelegramBot extends TelegramLongPollingBot {
+
+	private final String BOT_USERNAME = "anti_spam_tg_channels_bot";  // Replace with your bot username
+	private final String BOT_TOKEN = "7753305953:AAEFJcit9w62W9YAjaDqGhnM_5Ewcqz1gc8";  // Replace with your bot token
+
+	@Override
+	public void onUpdateReceived(Update update) {
+		// Check if the update contains a message
+		if (update.hasMessage()) {
+			Message message = update.getMessage();
+
+			// Create a SendMessage object to reply to the message
+			SendMessage response = new SendMessage();
+			response.setChatId(message.getChatId().toString());  // Make sure to pass the chat ID
+			response.setText("Hello, " + message.getFrom().getFirstName() + "! You said: " + message.getText());
+
+			try {
+				// Execute the message
+				execute(response);  // Sending the response to the user
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	@Override
+	public String getBotUsername() {
+		return BOT_USERNAME;
+	}
+
+	@Override
+	public String getBotToken() {
+		return BOT_TOKEN;
+	}
+}
---
+++ src/test/java/com/example/anti_spam_telegram_bot/AntiSpamTelegramBotTests.java
@@ -0,0 +1,13 @@
+package com.example.anti_spam_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiSpamTelegramBotTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
